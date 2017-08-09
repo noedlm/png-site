40a691ff402b484d2aba34ebb36dc5ce376079fd
Noe De La Mora <Noe.DeLaMora@univarusa.com>
2017-08-09 16:27:04 -0500

    navbar shinannigans
    - Added bootstrap 4.0 CDN
    - created a navbar component. Probably won't need a navbar but w/e

---
+++ src/Navigation.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Navigation extends Component {
+  render() {
+    let variable = "bar here";
+    return (
+        <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">
+                P/N
+            </a>
+            <div className="container">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">item 1</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">item 2</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">item 3</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+  }
+}
+
+export default Navigation;
+
