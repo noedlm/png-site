6650f127a1b8e89071992dd28513d6ca31a7bd41
Paul Grugnale <pago88@icloud.com>
2017-10-05 22:40:10 -0500

    Changes to Navigation. Also, components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import Navigation from './Navigation';
 import AboutUs from './AboutUs';
 import Team from './Team';
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
+import Scrollchor from 'react-scrollchor';
 
 class Navigation extends Component {
   render() {
     let variable = "bar here";
     return (
         <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+            {/* wtf is happening here?
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="#">
-                BSS
-            </a>
+            */} 
+            <Scrollchor to="" className="navbar-brand">BSS</Scrollchor>
             <div className="container">
                 <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#about-us">About</a>
+                            <Scrollchor to="#about-us" className="nav-link">About</Scrollchor>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#projects">Team</a>
+                            <Scrollchor to="#projects" className="nav-link">Team</Scrollchor>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#clients">Clients</a>
+                            <Scrollchor to="#clients" className="nav-link">Clients</Scrollchor>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact-us">Contact Us</a>
+                            <Scrollchor to="#contact-us" className="nav-link">Contact Us</Scrollchor>
                         </li>
                     </ul>
                 </div>
